6b8fe27b1b9f4b837b4cd457f481083f24cb22ca
Matthijs20 <matthijsl.westland@gmail.com>
2023-01-11 11:55:14 +0100

    toolchange code toegevoegd

--- Code/main.py
+++ Code/main.py
@@ -76,6 +76,7 @@ def achterklep2():
     ax = 20
     set_digital_output(2, 1)
     movel(vork_S1, v=velocity, a=acceleration)
+    wait(2)
     movel(vork_S2, v=vx, a=ax)
     movel(vork_S3, v=vx, a=ax)
     movel(vork_S4, v=vx, a=ax)
@@ -215,17 +216,59 @@ def voorrand_demonteren():
 
 
 
+m = 5
+def grab_tool(tool):
+    if tool == 1:
+        pos_boven_j = posj(5.8, 52.5, 41.5, 0.0, 85.9, 7.5 - 180)
+        pos_t = posx(687.3, 69.8, 168.2, 5.8, 180.0, -172.5)
+        pos_rotated_j = posj(5.8, 55.2, 49.6, 0.0, 75.2, 7.5 + 45 - 180)
+        pos_boven_rotated_t = posx(687.3, 69.8, 251.9, 5.8, 180.0, -127.6)
+    elif tool == 2:
+        pos_boven_j = posj(-1.7, 51.9, 43.4, -0.0, 84.7, -180.0)
+        pos_t = posx(687.1, -20.4, 168.6, 177.9, -180.0, -0.3)
+        pos_rotated_j = posj(-1.7, 54.6, 50.7, 0.0, 74.7, -180.0 + 45)
+        pos_boven_rotated_t = posx(687.1, -20.4, 247.0, 178.5, 180.0, 44.0)
+
+    movej(pos_boven_j, a=10, v=10 * m)
+    movel(pos_t, a=10, v=10 * m)
+    # Cilinder
+    movej(pos_rotated_j, a=10, v=10 * m)
+    movel(pos_boven_rotated_t, a=10, v=10 * m)
+    movej(home_pos_j, v=velocity, a=acceleration)
+
+
+def dump_tool(tool):
+    if tool == 1:
+        pos_boven_t = posx(687.3, 69.8, 251.9, 5.8, 179.9, -172.5)
+        pos_j = posj(5.8, 55.2, 49.6, 0.0, 75.2, -180)
+        pos_rotated_t = posx(687.3, 69.8, 168.2, 5.8, 180.0, -127.6)
+        pos_boven_rotated_j = posj(5.8, 52.5, 41.5, 0.0, 85.9, 7.5 + 45 - 180)
+    elif tool == 2:
+        pos_boven_t = posx(687.0, -20.4, 247.0, 1.2, -180.0, -178)
+        pos_j = posj(-1.7, 54.6, 50.7, 0.0, 74.7, -180.0)
+        pos_rotated_t = posx(687.1, -20.4, 168.5, 177.3, 180.0, 44.0)
+        pos_boven_rotated_j = posj(-1.7, 51.9, 43.4, -0.0, 84.7, -180.0 + 45)
+
+    movej(pos_boven_rotated_j, a = 10, v = 10 * m)
+    movel(pos_rotated_t, a = 10, v = 10 * m)
+    # Cilinder
+    movej(pos_j, a = 10, v = 10 * m)
+    movel(pos_boven_t, a=10,v=10*m)
+    movej(home_pos_j, v=velocity, a=acceleration)
+
+
 #main
 
 set_digital_output(1,0)
 movej(home_pos_j,v=velocity, a=acceleration)
-#achterklep()
-#wait(8)
-achterklep2()
-wait(8)
-achterklep3()
-#omdraaistation()
-#voorrand_demonteren()
-#batterij()
-
-set_digital_output(1,0)
+grab_tool(1) #vacuum opppakken
+achterklep() #Oppakken vanaf de tray
+dump_tool(1) #vacuum wegzetten
+grab_tool(2) #spatel oppakken
+achterklep2() #deel met de spatel
+dump_tool(2) #spatel wegzetten
+grab_tool(1) #vacuum oppakken
+achterklep3() #achterklep weghalen
+batterij() #batterij weghalen
+omdraaistation() #omdraaien
+voorrand_demonteren() #voorrand wegdrukken
